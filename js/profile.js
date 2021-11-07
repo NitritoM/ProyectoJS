ae2bdfeed02d0f97b4fd54afae1a9a2d0cf8e771
NitritoM <aressz@gmail.com>
2021-11-07 16:45:47 -0500

    Entrega Final

--- js/profile.js
+++ js/profile.js
@@ -10,3 +10,42 @@ document.getElementById('txtNombre').value  = auxAlumno.nombre;
 document.getElementById('txtApellido').value  = auxAlumno.apellidos;
 document.getElementById('txtDNI').value  = auxAlumno.dni;
 
+function cursosInscritos(){
+	if (auxAlumno.cursos[0]){
+		document.getElementById('txtMatematica').value = "Inscrito";
+	}
+	else{
+		document.getElementById('txtMatematica').value = "No Inscrito";
+	}
+
+	if (auxAlumno.cursos[1]){
+		document.getElementById('txtHistoria').value = "Inscrito";
+	}
+	else{
+		document.getElementById('txtHistoria').value = "No Inscrito";
+	}
+
+	if (auxAlumno.cursos[2]){
+		document.getElementById('txtComu').value = "Inscrito";
+	}
+	else{
+		document.getElementById('txtComu').value = "No Inscrito";
+	}
+
+	if (auxAlumno.cursos[3]){
+		document.getElementById('txtEcono').value = "Inscrito";
+	}
+	else{
+		document.getElementById('txtEcono').value = "No Inscrito";
+	}
+
+	if (auxAlumno.cursos[4]){
+		document.getElementById('txtProgra').value = "Inscrito";
+	}
+	else{
+		document.getElementById('txtProgra').value = "No Inscrito";
+	}
+}
+
+cursosInscritos();
+
